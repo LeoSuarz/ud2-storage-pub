d9a02fae9a84f66f239b59cb8247b4b0ad07e603
LeoSuarz <leo.suarzz05@gmail.com>
2024-11-29 21:30:37 +0100

    csv

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\JsonController;
+use App\Http\Controllers\CsvController;
 
 Route::apiResource('hello', HelloWorldController::class);
-
-use App\Http\Controllers\JsonController;
-
-Route::get('/json', [JsonController::class, 'index']);
-Route::post('/json', [JsonController::class, 'store']);
-Route::get('/json/{id}', [JsonController::class, 'show']);
-Route::put('/json/{id}', [JsonController::class, 'update']);
-Route::delete('/json/{id}', [JsonController::class, 'destroy']);
-
+Route::apiResource('json', JsonController::class);
+Route::apiResource('csv', CsvController::class);
